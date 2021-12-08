8c6abb6b7325fd04fd42d88910d85885dd49adb8
ZeyAzee <zhenyaletha@gmail.com>
2021-12-08 12:48:37 +0300

    add main method

---
+++ lab4/src/com/company3/Main.java
@@ -0,0 +1,15 @@
+package com.company3;
+
+import javax.swing.*;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Создать и показать экземпляр главного окна приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+}
